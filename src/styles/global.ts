6336de8f0f187fc4a27a7a1aadb0001088ee7448
André Graciano <andregraciano16@gmail.com>
2020-08-22 23:44:58 -0300

    adicionando link da font e corrigindo css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: "3112E38;
+    background: #312E38;
     color: #FFF;
     -webkit-font-smoothing: antialiased;
   }
@@ -17,6 +17,10 @@ export default createGlobalStyle`
   body, input, button {
     font-family: 'Roboto Slab', serif;
     font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
   }
 
   button {
