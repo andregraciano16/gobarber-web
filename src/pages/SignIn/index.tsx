95ac45cb72546588c1f945455cd24fc13ef61776
André Graciano <andregraciano16@gmail.com>
2020-08-30 23:46:41 -0300

    adicionando estrutura do context toast

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,7 +5,9 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
 import getValidationErrors from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.svg';
@@ -25,30 +27,36 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         email: Yup.string().email('Digite um e-mail válido').required('E-mail obrigatório'),
         password: Yup.string().required('Senha obrigatória'),
       });
+
       await schema.validate(data, {
         abortEarly: false,
       });
-      signIn({
+
+      await signIn({
         email: data.email,
         password: data.password
       });
+
     } catch (err) {
 
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
         formRef.current?.setErrors(errors);
       }
+      addToast();
 
     }
-  }, [signIn]);
+  }, [ signIn, addToast ]);
   return (
     <Container>
 
