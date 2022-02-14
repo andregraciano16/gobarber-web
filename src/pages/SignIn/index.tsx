31fc12e9b4c65e048c92769cd1f19ff9e6d04133
andregraciano16 <andregraciano16@gmail.com>
2022-02-14 18:58:05 -0300

    adicionando header do dashboard

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -31,74 +31,64 @@ const SignIn: React.FC = () => {
   const { addToast } = useToast();
   const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: SignInFormData) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        email: Yup.string().email('Digite um e-mail válido').required('E-mail obrigatório'),
-        password: Yup.string().required('Senha obrigatória'),
-      });
+        const schema = Yup.object().shape({
+          email: Yup.string().email('Digite um e-mail válido').required('E-mail obrigatório'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      await signIn({
-        email: data.email,
-        password: data.password
-      });
-      history.push('/dashboard')
-    } catch (err) {
-
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
-        return ;
+        await signIn({
+          email: data.email,
+          password: data.password
+        });
+        history.push('/dashboard')
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+          return ;
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais'
+        });
       }
-      addToast({
-        type: 'error',
-        title: 'Erro na autenticação',
-        description: 'Ocorreu um erro ao fazer login, cheque as credenciais'
-      });
-
-    }
   }, [ signIn, addToast, history ]);
   return (
     <Container>
-
       <Content>
         <AnimationContainer>
-            <img src={logoImg} alt="GoBaber" />
+          <img src={logoImg} alt="GoBaber" />
 
-            <Form ref={formRef} onSubmit={handleSubmit}>
-              <h1>Faça seu login</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu login</h1>
 
-              <Input
-                name="email"
-                icon={FiMail}
-                placeholder="E-mail" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-              <Input
-                name="password"
-                icon={FiLock}
-                type="password"
-                placeholder="Senha" />
+            <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-              <Button type="submit">Entrar</Button>
-              <a href="forgot">Esqueci minha senha</a>
-            </Form>
-            <Link to="/signup">
-              <FiLogIn />
+            <Button type="submit">Entrar</Button>
+            <a href="/forgot-password">Esqueci minha senha</a>
+          </Form>
+          <Link to="/signup">
+            <FiLogIn />
             Criar conta
           </Link>
         </AnimationContainer>
       </Content>
 
       <Background />
-
     </Container>
   );
-}
+};
 
 export default SignIn;
