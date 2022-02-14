059f91d677444de312b5c799f20402d98439b038
andregraciano16 <andregraciano16@gmail.com>
2022-02-14 18:56:09 -0300

    criando reset e forgot password

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,8 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignOut from '../pages/SignUp';
+import ResetPassword from '../pages/ResetPassword';
+import ForgotPassword from '../pages/ForgotPassword';
 
 import Dashboard from '../pages/Dashboard';
 
@@ -12,7 +14,10 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn}/>
     <Route path="/signup" component={SignOut}/>
+    <Route path="/forgot-password" component={ForgotPassword}/>
     <Route path="/dashboard" component={Dashboard} isPrivate/>
+    <Route path="/reset-password" component={ResetPassword}/>
+
   </Switch>
 );
 
