8377d562b92fa67e1aa1e30441d2f2e7afc9cfb5
andregraciano16 <andregraciano16@gmail.com>
2022-02-14 18:57:17 -0300

    Alterando boolean por number para o style

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -34,8 +34,8 @@ const Toast: React.FC<ToastProps> = ({ message, style }) => {
   return (
 
     <Container type={message.type}
-               hasDescription={!!message.description}
-               style={style}>
+      hasDescription={Number(!!message.description)}
+      style={style}>
       { icons[message.type || 'info']}
       <FiAlertCircle size={20} />
 
